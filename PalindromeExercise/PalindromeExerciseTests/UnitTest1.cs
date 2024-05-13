41b173cb7e841cb1c01798fbd1ed217caae2bde6
davidmrtn07 <davidmrtn07@gmail.com>
2024-05-13 12:58:55 -0500

    Part 2 complete

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,24 @@
 using System;
+using PalindromeExercise;
+using PalindromeExerciseTests;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var wordSmith = new WordSmith();
+            //act
+            var actual = wordSmith.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
